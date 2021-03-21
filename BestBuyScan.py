1505b8e23274e5fe264631d8986bc54b533cde9c
Peter Calabrese <peter@thirtyfour.tech>
2021-03-21 11:40:43 -0400

    made card search modular, allows user to enter card model

--- BestBuyScan.py
+++ BestBuyScan.py
@@ -4,12 +4,12 @@ from SendAlert import SendAlert
 class BestBuyScanner:
     def __init__(self):
         self.sa = SendAlert()
-    def getJSONPageCount(self,key):
-        url=f'https://api.bestbuy.com/v1/products(search=rtx&search=3070&search=graphics&search=card)?format=json&show=sku&apiKey={key}'
+    def getJSONPageCount(self,key,card):
+        url=f'https://api.bestbuy.com/v1/products(search=rtx&search={card}&search=graphics&search=card)?format=json&show=sku&apiKey={key}'
         r=requests.get(url)
         self.pageCount= r.json()["totalPages"]
 
-    def scanBBStock(self, key, email, password,notifyContact, port):
+    def scanBBStock(self, key, card, email, password,notifyContact, port):
         
         x=0
         count =0
@@ -17,14 +17,14 @@ class BestBuyScanner:
         if(self.pageCount>=1):
             for i in range(self.pageCount):
                 x=i+1
-                url=f'https://api.bestbuy.com/v1/products(search=rtx&search=3070&search=graphics&search=card)?format=json&show=sku,name,salePrice,orderable,url&apiKey={key}&page={x}'
+                url=f'https://api.bestbuy.com/v1/products(search=rtx&search={card}&search=graphics&search=card)?format=json&show=sku,name,salePrice,orderable,url&apiKey={key}&page={x}'
                 w.append(requests.get(url))
         for c in range(len(w)):   
             for j in range(len(w[c].json())):
                 try:
                     print(f'''Product: {w[c].json()["products"][j]["name"]}\nPrice: {w[c].json()["products"][j]["salePrice"]}\nAvailability: {w[c].json()["products"][j]["orderable"]}\nURL:{w[c].json()["products"][j]["url"]}\n=============================================================================================================''')
                     count+=1
-                    if (w[c].json()["products"][j]["orderable"] == "Available"):
+                    if (w[c].json()["products"][j]["orderable"] == "SoldOut"):
                         card = w[c].json()["products"][j]["name"]
                         url= w[c].json()["products"][j]["url"]
                         self.sa.sendAlert(email, password,notifyContact,port,card,url)
